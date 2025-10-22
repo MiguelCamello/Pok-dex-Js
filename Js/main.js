fc33f2040357c1c65b93a7204b2ea6988f309d35
MiguelLima <mlimacamello@gmail>
2025-10-22 14:48:52 -0300

    termino do html e começo do main.js

---
+++ Js/main.js
@@ -0,0 +1,31 @@
+let topo;
+
+let loading = false;
+let fltered = false;
+
+const url = "https://pokeapi.co/api/v2";
+
+const allTypes = [
+    "steel",
+    "water",
+    "dragon",
+    "electric",
+    "fairy",
+    "ghost",
+    "fire",
+    "ice",
+    "bug",
+    "fighting",
+    "normal",
+    "rock",
+    "grass",
+    "psychic",
+    "dark",
+    "ground",
+    "poison",
+    "flying",
+];
+
+const todosTipos = [
+    { nome: "aço", cor: "rgba(170, 170, 187, 0.75)"},
+]
